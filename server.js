e71b91e30ad9e1e47741340c30f450444b1d4182
rabin001 <rabin.gora@gmail.com>
2019-02-27 11:51:49 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+
+var express = require("express");
+var app = express();
+
+
+// this is the line that tells our server to use the "/static" folder for static content
+app.use(express.static(__dirname + "/static"));
+// two underscores before dirname
+// try printing out __dirname using console.log to see what it is and why we use it
+
+
+app.listen(8000, function () {
+})
